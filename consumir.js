62ef5845885c674f8285b2063ca33e94b1677cce
datascienceurc <victor.cruz.arrona@gmail.com>
2023-12-11 22:04:18 -0600

    se agrega caja de busqueda

--- consumir.js
+++ consumir.js
@@ -1,10 +1,12 @@
 
 $(document).ready(function() {
   cargarDatos();
+  var texto = $("#buscador").val();
 });
 
 function  cargarDatos() { 
-      var tabla = $("<table class=\"table\"></table>"); // Crear el elemento <table>
+      
+      var tabla = $("<table id = \"tabla\"  class=\"table  \"></table>"); // Crear el elemento <table>
       var encabezado = $("<tr></tr>"); // Crear el elemento <tr> para el encabezado
       encabezado.append("<th scope =\"col\">Nombre</th>"); // Agregar la celda <th> para el nombre
       encabezado.append("<th scope =\"col\">Genero</th>"); // Agregar la celda <th> para la edad
@@ -14,13 +16,13 @@ function  cargarDatos() {
       $.getJSON("https://hp-api.onrender.com/api/characters")
       .done(function(data) {
         $.each(data, function(index, item) {   
-          console.log('hola1') ;
-          //$("#contenedor").html(item.name);
-          var fila = $("<tr></tr>"); // Crear el elemento <tr> para la filavar fila = $("<tr></tr>"); // Crear el elemento <tr> para la fila
+          var fila = $("<tr></tr>"); // Crear el elemento <tr> 
           fila.append("<td>"+item.name+"</td>"); // Agregar la celda <td> para el nombre
           fila.append("<td>"+item.gender+"</td>"); // Agregar la celda <td> para el genero
-          fila.append("<td><img src ="+item.image+" height=\"120px\" width =\"80px\" onerror= \"this.src='images/default.jpg'\" ></td>"); // Agregar la celda <td> para el genero
-          tabla.append(fila); // Agregar la fila a la tabla
+          fila.append("<td><img src ="+item.image+" height=\"120px\" width =\"80px\" onerror= \"this.src='images/default.jpg'\"  class=\"img-thumbnail\" ></td>"); // Agregar la celda <td> para el genero
+          var body =$("<tbody></tbody>");
+          body.append(fila);
+          tabla.append(body); // Agregar la fila a la tabla
         });  
         $("#contenedor").append(tabla); // Agregar la tabla al elemento con id="contenedor"
       })
@@ -28,6 +30,8 @@ function  cargarDatos() {
         // Se ha producido un error al llamar a la API
         console.log(error);
       });
+   
+
 }
 
 
